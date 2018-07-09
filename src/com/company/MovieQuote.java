aa7bcef54da23f69d8cfe107be045a50291a2c88
robert daniel cook <robertdaniel.cook@my.jcu.edu.com>
2018-07-09 14:34:18 +0800

    Pick 2 exercise

---
+++ src/com/company/MovieQuote.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class MovieQuote
+    {
+        public static void main(String[] args)
+        {
+            System.out.println("Say what again. SAY WHAT again! And I dare you, I double dare you motherfucker! Say what one more time.");
+
+        }
+    }
+
---
+++ src/com/company/TableAndChair.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class TableAndChair {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("x                     x");
+        System.out.println("x                     x");
+        System.out.println("x      xxxxxxxxx      x");
+        System.out.println("xxxxxx x       x xxxxxx");
+        System.out.println("x    x x       x x    x");
+        System.out.println("x    x x       x x    x");
+
+    }
+}
